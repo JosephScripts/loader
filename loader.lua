4d3502446ce94be62fa66c4c28b8f86e3a57ae89
JosephScripts <249978121+JosephScripts@users.noreply.github.com>
2026-04-08 20:39:53 +0200

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,14 @@
+-- ABA 1458767429
+if game.PlaceId == 1458767429 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JosephScripts/aba/refs/heads/main/aba.txt"))()
+end
+
+-- jujutsu randomizer 95467791496576
+if game.PlaceId == 95467791496576 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JosephScripts/jujutsurandomizer/refs/heads/main/jjr.txt"))()
+end
+
+-- arsenal 286090429
+if game.PlaceId == 286090429 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JosephScripts/arsenal/refs/heads/main/arsenal.lua"))()
+end
